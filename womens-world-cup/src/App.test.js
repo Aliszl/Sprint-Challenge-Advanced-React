df1689890999d74b2df31db05df52c06547ac846
Alison Law <alisparklaw@gmail.com>
2019-12-06 10:18:55 +0000

    configure tests

--- womens-world-cup/src/App.test.js
+++ womens-world-cup/src/App.test.js
@@ -1,6 +1,6 @@
 import React from "react";
-// import { render } from '@testing-library/react';
 import App from "./App";
+import WorldcupData from "./components/Worldcupdata";
 
 import * as rtl from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
@@ -8,12 +8,12 @@ import "@testing-library/jest-dom/extend-expect";
 afterEach(rtl.cleanup);
 
 it("renders without crashing", () => {
-  const wrapper = rtl.render(<App />);
+  const wrapper = rtl.render(<WorldcupData />);
+  wrapper.debug();
 });
 it('renders "Womens World Cup" text', () => {
-  const wrapper = rtl.render(<App />);
-  // IMPORTANT
-  // wrapper.queryByText() returns either the node, or null:
+  const wrapper = rtl.render(<WorldcupData />);
+  wrapper.debug();
   const headerText = wrapper.queryByText(/Womens World Cup/i);
   expect(headerText).toBeInTheDocument();
 });
