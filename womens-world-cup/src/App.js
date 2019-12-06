6dc7c2cec3999baae45d14bc38e75edec85d00c5
Alison Law <alisparklaw@gmail.com>
2019-12-06 08:15:38 +0000

    File set up with create react app.  Model app tidy and Answers.md completed

---
+++ womens-world-cup/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Women's Word Cup</h1>
+    </div>
+  );
+}
+
+export default App;
